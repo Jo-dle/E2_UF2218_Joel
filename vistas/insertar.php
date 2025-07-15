4f3ee22138bd6582f78931c39ed8bcd7d9091994
Jo-dle <jramoszurita@gmail.com>
2025-07-15 11:36:38 +0100

    corrección de errores en las validaciones

--- vistas/insertar.php
+++ vistas/insertar.php
@@ -34,7 +34,7 @@
     <input type="hidden" name="accion" value="insertar">
     <div class="mb-3">
       <label class="form-label">Matrícula</label>
-      <input type="text" name="matricula" class="form-control" placeholder="Ej: 1234ABC" required min="0" max="99999">
+      <input type="text" name="matricula" class="form-control" required pattern="\d{4}[A-Z]{3}" placeholder="Ej: 1234ABC" required min="0" max="99999">
     </div>
     <div class="mb-3">
       <label class="form-label">Marca</label>
@@ -54,7 +54,7 @@
     </div>
     <div class="mb-3">
       <label class="form-label">Precio</label>
-      <input type="text" name="precio" class="form-control" placeholder="Ej: 012345€" required min="0" max="99999">
+      <input type="number" name="precio" class="form-control" required min="0" max="99999">
     </div>
     <div class="mb-3">
       <label class="form-label">Tipo de Venta</label>
